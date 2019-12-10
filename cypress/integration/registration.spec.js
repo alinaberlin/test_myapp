8478a32c22d3ca53be3be74393b397aa6dfc499b
Alina Ghetler <alina.ghetler@gmail.com>
2019-12-10 17:09:28 +0100

    initial commit

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -7,5 +7,6 @@ cy.get('input[name=firstName]').type("Alina")
 cy.get('input[name=lastName]').type("Ghetler")
 cy.get('input[type=email]').type("alina.ghetler@gmail.com")
 cy.get('input[type=password]').type("alina")
+cy.get('select').select('5deebb1888b1b3000315e363') 
 cy.get("button[class=post-user-button]").click();
 })
