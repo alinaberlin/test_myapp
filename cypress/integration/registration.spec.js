f9c82ca151a395a14d2a5b54038649db0b9a22eb
Alina Ghetler <alina.ghetler@gmail.com>
2019-12-11 00:16:54 +0100

    Full flow

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -1,12 +1,25 @@
 ///<reference types="cypress"/>
 
-
-it('should make the registration',()=>{
-cy.visit("https://sportamicii-alinaghetler-fe.herokuapp.com/registration")
-cy.get('input[name=firstName]').type("Alina")
-cy.get('input[name=lastName]').type("Ghetler")
-cy.get('input[type=email]').type("alina.ghetler@gmail.com")
-cy.get('input[type=password]').type("alina")
-cy.get('select').select('5deebb1888b1b3000315e363') 
-cy.get("button[class=post-user-button]").click();
-})
+it("should make the registration", () => {
+    cy.visit("https://sportamicii-alinaghetler-fe.herokuapp.com/registration");
+    cy.get("input[name=firstName]").type("Alina");
+    cy.get("input[name=lastName]").type("Ghetler");
+    cy.get("input[type=email]").type("alina04.ghetler@gmail.com");
+    cy.get("input[type=password]").type("alina");
+    cy.get("select").select("5deebb1888b1b3000315e363");
+    cy.get("button").click();
+    cy.wait(500);
+    cy.get("input[name=email]").type("alina04.ghetler@gmail.com");
+    cy.get("input[type=password]").type("alina");
+    cy.get("button").click();
+    cy.wait(500);
+    cy.get("a")
+        .contains("Add Event")
+        .click();
+    cy.wait(500);
+    cy.get("input[name=title]").type("Handball");
+    cy.get("input[name=date]").type("2019-12-22");
+    cy.get("select[name=venues]").select("5deebb9188b1b3000315e365");
+    cy.get("select[name=sports]").select("5deebb1888b1b3000315e363");
+    cy.get("button").click();
+});
